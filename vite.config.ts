3c87fa033d68e6abd8cd0ddf7cbc3200e45325e3
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-10-16 02:26:21 -0300

    build: remove dist-electron folder before each build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import fs from "node:fs";
+import { join } from "node:path";
+
+/**
+ * Remove the dist-electron folder before building
+ */
+const removeElectronDist = () => {
+  const DIST_PATH = join(__dirname, "dist-electron");
+  console.log(`Removing ${DIST_PATH} before building...`);
+  fs.rmSync(DIST_PATH, { recursive: true, force: true });
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
